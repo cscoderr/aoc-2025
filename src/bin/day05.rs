5035eea7f0db87c846ed896d50b419a87e47bcf8
cscoderr <tomiwaidowuu@gmail.com>
2025-12-08 13:06:57 +0100

    feat: add day05 part2 and day06 part1

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fs};
+use std::{cmp::max, collections::HashSet, fs};
 
 fn part_one(input: &str) -> i64 {
     let parts: Vec<&str> = input.split("\n\n").collect();
@@ -41,17 +41,32 @@ fn part_one(input: &str) -> i64 {
     count
 }
 
+fn merge_ranges(mut ranges: Vec<(i64, i64)>) -> Vec<(i64, i64)> {
+    if ranges.is_empty() {
+        return vec![];
+    }
+    ranges.sort_by_key(|r| r.0);
+
+    let mut merged = vec![ranges[0]];
+
+    for (start, end) in ranges.into_iter().skip(1) {
+        let last = merged.last_mut().unwrap();
+        if start <= last.1 {
+            last.1 = max(last.1, end);
+        } else {
+            merged.push((start, end));
+        }
+    }
+
+    merged
+}
+
 fn part_two(input: &str) -> i64 {
     let parts: Vec<&str> = input.split("\n\n").collect();
     if parts.len() < 2 {
         return 0;
     }
     let ranges = parts[0];
-    let available_ingredients = parts[1];
-    let ingredients: HashSet<i64> = available_ingredients
-        .lines()
-        .filter_map(|s| s.trim().parse::<i64>().ok())
-        .collect();
 
     let ranges: Vec<(i64, i64)> = ranges
         .lines()
@@ -63,24 +78,17 @@ fn part_two(input: &str) -> i64 {
             })
         })
         .collect();
-    let mut count = 0;
-    for ingredient_id in ingredients.iter() {
-        let mut is_covered = false;
-
-        for (start, end) in &ranges {
-            if ingredient_id >= start && ingredient_id <= end {
-                is_covered = true;
-                break;
-            }
-        }
-
-        if is_covered {
-            count += 1;
-        }
+    let merged = merge_ranges(ranges);
+    let mut result = Vec::new();
+    for (start, end) in merged {
+        let n = end - start + 1;
+        result.push(n);
     }
 
-    count
+    result.iter().sum()
 }
+// Day05, Part1: 789
+// Day05, Part2: 343329651880509
 fn main() {
     let input = fs::read_to_string("inputs/day05.txt").unwrap_or_default();
     println!("Day05, Part1: {}", part_one(&input));
--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,1 +1,42 @@
-fn main() {}
+use std::fs;
+
+fn calculate_from_operator_str(operator_str: &str, a: i64, b: i64) -> i64 {
+    let final_result = match operator_str {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        _ => {
+            eprintln!("Error: Unrecognized operator: {}", operator_str);
+            return 0;
+        }
+    };
+    final_result
+}
+fn part_one(input: &str) -> i64 {
+    let mut lines: Vec<Vec<&str>> = input
+        .lines()
+        .map(|line| line.split_whitespace().collect())
+        .collect();
+    let symbols = lines.pop().unwrap();
+    let mut totals: Vec<i64> = Vec::new();
+    for col in 0..lines[0].len() {
+        let first_value = lines[0][col].parse::<i64>().unwrap();
+        let mut result = first_value;
+        for row in 1..lines.len() {
+            let value = lines[row][col].parse::<i64>().unwrap();
+            result = calculate_from_operator_str(symbols[col], result, value);
+        }
+        totals.push(result);
+    }
+    totals.iter().sum()
+}
+
+fn part_two(input: &str) -> i64 {
+    0
+}
+//3261038365331
+fn main() {
+    let input = fs::read_to_string("inputs/day06.txt").unwrap_or_default();
+    println!("Day06, Part1: {}", part_one(&input));
+    println!("Day06, Part2: {}", part_two(&input));
+}
